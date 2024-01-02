9970ebdd5e9c059dfa5d071a632abe8e734429e4
ChandraShekhar <csyadav0513@gmail.com>
2024-01-02 14:20:27 +0530

    fixed bugs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Todo App",
   description: "Todo App made with Next.js and Firebase",
-  image: "/images/logo.png",
 };
 
 export default function RootLayout({ children }) {
